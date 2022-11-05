556f1520457056c40f45633976dbd0b4ce57fc52
yayanrw <yayanraw@gmail.com>
2022-11-05 13:50:04 +0700

    add SearchBar.kt components

---
+++ app/src/main/java/com/heyproject/jetcoffee/ui/components/SearchBar.kt
@@ -0,0 +1,56 @@
+package com.heyproject.jetcoffee.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.heyproject.jetcoffee.R
+import com.heyproject.jetcoffee.ui.theme.JetCoffeeTheme
+
+/**
+Written by Yayan Rahmat Wijaya on 11/5/2022 13:32
+Github : https://github.com/yayanrw
+ **/
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "")
+        }, colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ), placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .clip(RoundedCornerShape(16.dp))
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchBarPreview() {
+    JetCoffeeTheme {
+        SearchBar()
+    }
+}
