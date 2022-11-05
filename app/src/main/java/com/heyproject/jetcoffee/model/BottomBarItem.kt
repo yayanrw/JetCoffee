5dfa50e7ba3c4eeb24213033bafbc0d3600ac517
yayanrw <yayanraw@gmail.com>
2022-11-05 14:54:10 +0700

    add BottomBarItem.kt

---
+++ app/src/main/java/com/heyproject/jetcoffee/model/BottomBarItem.kt
@@ -0,0 +1,10 @@
+package com.heyproject.jetcoffee.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+/**
+Written by Yayan Rahmat Wijaya on 11/5/2022 14:53
+Github : https://github.com/yayanrw
+ **/
+
+data class BottomBarItem(val title: String, val icon: ImageVector)
