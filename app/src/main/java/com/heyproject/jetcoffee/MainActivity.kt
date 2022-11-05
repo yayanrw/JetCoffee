18a7e1825386f0037524b24bf71e9e53598dad2d
yayanrw <yayanraw@gmail.com>
2022-11-05 14:13:41 +0700

    preview categoryrow

--- app/src/main/java/com/heyproject/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/heyproject/jetcoffee/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -14,6 +14,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.heyproject.jetcoffee.model.dummyCategory
+import com.heyproject.jetcoffee.ui.components.CategoryItem
 import com.heyproject.jetcoffee.ui.components.SearchBar
 import com.heyproject.jetcoffee.ui.theme.JetCoffeeTheme
 
@@ -50,8 +52,30 @@ fun Banner(
     }
 }
 
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
 
-@Preview(showBackground = true, device = Devices.PIXEL_4, showSystemUi = true)
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview() {
+    JetCoffeeTheme {
+        CategoryRow()
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
 fun JetCoffeeAppPreview() {
     JetCoffeeTheme {
