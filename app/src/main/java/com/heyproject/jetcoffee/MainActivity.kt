7f2bae52ba8326378987eb2a86f38c5c51a15ef7
yayanrw <yayanraw@gmail.com>
2022-11-05 14:05:04 +0700

    show banner

--- app/src/main/java/com/heyproject/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/heyproject/jetcoffee/MainActivity.kt
@@ -3,9 +3,18 @@ package com.heyproject.jetcoffee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.heyproject.jetcoffee.ui.components.SearchBar
 import com.heyproject.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,10 +30,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
 }
 
 
-@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Preview(showBackground = true, device = Devices.PIXEL_4, showSystemUi = true)
 @Composable
 fun JetCoffeeAppPreview() {
     JetCoffeeTheme {
