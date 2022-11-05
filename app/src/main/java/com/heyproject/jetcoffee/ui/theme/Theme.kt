1090e6d1b41ed7576dae247cec07f55d4fb51b3d
yayanrw <yayanraw@gmail.com>
2022-11-05 15:01:14 +0700

    add theming

--- app/src/main/java/com/heyproject/jetcoffee/ui/theme/Theme.kt
+++ app/src/main/java/com/heyproject/jetcoffee/ui/theme/Theme.kt
@@ -7,15 +7,11 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = LightCoffeeBrown, primaryVariant = LightCoffeeBrown, secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = CoffeeBrown, primaryVariant = CoffeeBrown, secondary = Teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -36,9 +32,6 @@ fun JetCoffeeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composa
     }
 
     MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
+        colors = colors, typography = Typography, shapes = Shapes, content = content
     )
 }
