838a74f93a38159deba5ac2ca9efb4fccbb0a43b
yayanrw <yayanraw@gmail.com>
2022-11-05 14:35:32 +0700

    add menu

--- app/src/main/java/com/heyproject/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/heyproject/jetcoffee/MainActivity.kt
@@ -15,8 +15,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.heyproject.jetcoffee.model.Menu
+import com.heyproject.jetcoffee.model.dummyBestSellerMenu
 import com.heyproject.jetcoffee.model.dummyCategory
+import com.heyproject.jetcoffee.model.dummyMenu
 import com.heyproject.jetcoffee.ui.components.CategoryItem
+import com.heyproject.jetcoffee.ui.components.MenuItem
 import com.heyproject.jetcoffee.ui.components.SearchBar
 import com.heyproject.jetcoffee.ui.components.SectionText
 import com.heyproject.jetcoffee.ui.theme.JetCoffeeTheme
@@ -38,6 +42,10 @@ fun JetCoffeeApp() {
         Banner()
         SectionText(stringResource(R.string.section_category))
         CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
     }
 }
 
@@ -72,10 +80,34 @@ fun CategoryRow(
 }
 
 @Composable
+fun MenuRow(
+    menus: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(menus, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Composable
 @Preview(showBackground = true)
 fun CategoryRowPreview() {
     JetCoffeeTheme {
         CategoryRow()
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun MenuRowPreview() {
+    JetCoffeeTheme {
+        MenuRow(menus = dummyMenu)
     }
 }
 
