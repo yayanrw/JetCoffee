e18684c726f7d557e5898f581e72efc7059c1836
yayanrw <yayanraw@gmail.com>
2022-11-05 14:58:53 +0700

    add navabr

--- app/src/main/java/com/heyproject/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/heyproject/jetcoffee/MainActivity.kt
@@ -7,18 +7,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.heyproject.jetcoffee.model.Menu
-import com.heyproject.jetcoffee.model.dummyBestSellerMenu
-import com.heyproject.jetcoffee.model.dummyCategory
-import com.heyproject.jetcoffee.model.dummyMenu
+import com.heyproject.jetcoffee.model.*
 import com.heyproject.jetcoffee.ui.components.CategoryItem
 import com.heyproject.jetcoffee.ui.components.HomeSection
 import com.heyproject.jetcoffee.ui.components.MenuItem
@@ -38,20 +41,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Column {
-        Banner()
-        HomeSection(
-            title = stringResource(R.string.section_category),
-            content = { CategoryRow() }
-        )
-        HomeSection(
-            title = stringResource(R.string.section_best_seller_menu),
-            content = { MenuRow(dummyMenu) }
-        )
-        HomeSection(
-            title = stringResource(R.string.section_best_seller_menu),
-            content = { MenuRow(dummyBestSellerMenu) }
-        )
+    Scaffold(bottomBar = {
+        BottomBar()
+    }) {
+        Column {
+            Banner()
+            HomeSection(title = stringResource(R.string.section_category),
+                content = { CategoryRow() })
+            HomeSection(title = stringResource(R.string.section_best_seller_menu),
+                content = { MenuRow(dummyMenu) })
+            HomeSection(title = stringResource(R.string.section_best_seller_menu),
+                content = { MenuRow(dummyBestSellerMenu) })
+        }
     }
 }
 
@@ -87,8 +88,7 @@ fun CategoryRow(
 
 @Composable
 fun MenuRow(
-    menus: List<Menu>,
-    modifier: Modifier = Modifier
+    menus: List<Menu>, modifier: Modifier = Modifier
 ) {
     LazyRow(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
@@ -97,6 +97,40 @@ fun MenuRow(
     ) {
         items(menus, key = { it.title }) { menu ->
             MenuItem(menu)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(R.string.menu_home), icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite), icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile), icon = Icons.Default.AccountCircle
+            ),
+        )
+        navigationItems.map {
+            BottomNavigationItem(icon = {
+                Icon(
+                    imageVector = it.icon, contentDescription = it.title
+                )
+            },
+                label = {
+                    Text(it.title)
+                },
+                selected = it.title == navigationItems[0].title,
+                unselectedContentColor = LightGray,
+                onClick = {})
         }
     }
 }
